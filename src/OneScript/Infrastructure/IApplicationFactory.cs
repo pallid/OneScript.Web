18afd054bea03d27879422f09be39b4f82d2a685
pallid <1005220@mail.ru>
2018-08-08 01:43:54 +0300

    no message

--- src/OneScript/Infrastructure/IApplicationFactory.cs
+++ src/OneScript/Infrastructure/IApplicationFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using OneScript.WebHost.Application;
-//using OneScript.WebHost.Infobase;
 using ScriptEngine.Machine;
 
 namespace OneScript.WebHost.Infrastructure
